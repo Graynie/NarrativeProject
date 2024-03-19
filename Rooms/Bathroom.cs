c996115ed49c5656e0d39ad3cc3647508393026d
Graynielle <danielle.mercy.rod@gmail.com>
2024-03-19 13:32:52 -0400

    Add bool to know if Bath has been Done to Bathroom
    Created a condition to determine that the password in the mirror will only be visible if the bath has been done before, else it will only show the reflection.

--- Rooms/Bathroom.cs
+++ Rooms/Bathroom.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Runtime.Remoting.Channels;
 
 namespace NarrativeProject.Rooms
 {
     internal class Bathroom : Room
     {
-
+        internal static bool bathDone = false;
         internal override string CreateDescription() =>
 @"In your bathroom, the [bath] is filled with hot water.
 The [mirror] in front of you reflects your depressed face.
@@ -13,14 +14,25 @@ You can return to your [bedroom].
 
         internal override void ReceiveChoice(string choice)
         {
+           
             switch (choice)
             {
+
                 case "bath":
                     Console.WriteLine("You relax in the bath.");
+                   bathDone = true;
                     break;
                 case "mirror":
-                    Console.WriteLine("You see the numbers 2314 written on the fog on your mirror.");
+                    if (bathDone==true)
+                    {
+                        Console.WriteLine("You see the numbers 6595 written on the fog on your mirror.");
+                    }
+                    else if(bathDone==false)
+                    {
+                        Console.WriteLine("You see yourself in the mirror");
+                    }
                     break;
+                    
                 case "bedroom":
                     Console.WriteLine("You return to your bedroom.");
                     Game.Transition<Bedroom>();
