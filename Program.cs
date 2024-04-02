097b9dfca7ed29215af82a3f54855b8e052dea27
Graynielle <danielle.mercy.rod@gmail.com>
2024-04-02 13:48:55 -0400

    Add and Implement new rooms to the project
    add new rooms: Cellar, Closet, garage, Grocery Store, Kitchen.
    Implement them in Program cs file

--- Program.cs
+++ Program.cs
@@ -5,13 +5,20 @@ namespace NarrativeProject
 {
     internal class Program
     {
+       
         static void Main(string[] args)
         {
             var game = new Game();
+            game.Add(new LivingRoom());
+            game.Add(new Kitchen());
             game.Add(new Bedroom());
             game.Add(new Bathroom());
             game.Add(new AtticRoom());
-            game.Add(new LivingRoom());
+            game.Add(new Cellar());
+            game.Add(new Closet());
+            game.Add(new Garage());
+            game.Add(new Grocery());
+
 
             while (!game.IsGameOver())
             {
---
+++ Rooms/Kitchen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarrativeProject.Rooms
+{
+    internal class Kitchen : Room
+    {
+        internal override string CreateDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void ReceiveChoice(string choice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
